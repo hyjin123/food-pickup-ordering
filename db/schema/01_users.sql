74c498e7c3fc7de225f00322b267195803347023
Ana Carolina K <anacko@gmail.com>
2021-10-16 18:11:44 -0400

    add create tables queries

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,15 @@
--- Drop and recreate Users table (Example)
+DROP TABLE IF EXISTS customers CASCADE;
 
-DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
+CREATE TABLE customers (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  phone_number VARCHAR(255),
+  password TEXT NOT NULL,
+  address VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  postal_code VARCHAR(255),
+  member_since TIMESTAMP DEFAULT Now(),
+  is_active BOOLEAN DEFAULT TRUE
 );
--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -1,8 +1,29 @@
--- Drop and recreate Widgets table (Example)
+DROP TABLE IF EXISTS menu_items CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS order_items CASCADE;
 
-DROP TABLE IF EXISTS widgets CASCADE;
-CREATE TABLE widgets (
+CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  price REAL NOT NULL,
+  image_url TEXT NOT NULL,
+  in_stock BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
+  created_at TIMESTAMP DEFAULT Now(),
+  updated_at TIMESTAMP DEFAULT Now(),
+  prep_time INTERVAL DEFAULT '00:40:00.00',
+  note TEXT,
+  tip REAL DEFAULT 0
+);
+
+CREATE TABLE order_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+  menu_item_id INTEGER REFERENCES menu_items(id) ON DELETE CASCADE,
+  quantity INTEGER DEFAULT 1
 );
