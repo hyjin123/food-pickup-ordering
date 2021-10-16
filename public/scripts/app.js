8a9e5899ad404a460943193fb37d68676b571c33
Sean Jin <seanhoyeonjin@gmail.com>
2021-10-16 21:16:58 +0000

    Added a cart summary box and possible scroll to the top feature

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,20 @@
 // Client facing scripts here
+
+$(() => {
+
+  // feature that shows the toggle button based on the vertical position
+  $(document).scroll(function() {
+    let position = $(this).scrollTop();
+    if (position > 200) {
+      $("#toggle-button").fadeIn();
+    } else {
+      $("#toggle-button").fadeOut();
+    }
+  });
+
+  // feature that lets you scroll to the top when you toggle the button
+  $("#toggle-button").click(function() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  });
+
+});
