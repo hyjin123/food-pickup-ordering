d6dc45cd58a17b5c25b9a498840d24837325ef14
Thainnt <thai164@gmail.com>
2021-10-19 20:04:41 +0000

    commit updated progress

--- routes/orders.js
+++ routes/orders.js
@@ -10,6 +10,7 @@ express().use(cookieSession({
 module.exports = (db) => {
   router.post("/", (req, res) => {
     console.log('req: ',req.body);
+
     let query = `INSERT INTO orders (customer_id, created_at, note)
     VALUES ($1, now()::date, $2) RETURNING id ;`;
     db.query(query, [req.session.userId, req.body.note])
@@ -19,18 +20,33 @@ module.exports = (db) => {
       })
       .then(id => {
         console.log('order id: ',id);
-        // db.query(`
-        //   INSERT INTO order_items (order_id, menu_item_id, quantity)
-        //   VALUES ($1, now()::date, $2) RETURNING id ;`
-        //   [id, req.body.items[0].item_id])
-        //   .then(data => {
-        //     console.log(data);
-        //   })
-        //   .catch(err => {
-        //     res
-        //       .status(500)
-        //       .json({ error: err.message });
+        let queryParams = [];
+        let queryString = `
+        INSERT INTO order_items (order_id, menu_item_id, quantity)
+        VALUES `;
+
+        for (const index = 0; index < req.body.items.length; index++) {
+          queryString += `($${3 * index + 1}, $${3 * index + 2}, $${3 * index + 3})`;
+          if (index < req.body.items.length - 2) {
+            queryString +=`, `;
+          } else {
+            queryString += ` RETURNING *;`;
+          }
+          queryParams.push(id, req.body.items[index].item_id, req.body.items[index].quantity);
+        }
+
+        console.log('queryString; ',queryString, 'queryParams: ', queryParams );
+
+        return db.query(queryString, queryParams)
+          .then(data => {
+            console.log(data);
           })
+          .catch(err => {
+            res
+              .status(500)
+              .json({ error: err.message });
+          })
+      })
       .catch(err => {
         res
           .status(500)
