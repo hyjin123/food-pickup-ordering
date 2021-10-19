a600b9a351330555178d459e964fb5ac643e2247
Ana Carolina K <anacko@gmail.com>
2021-10-19 13:43:43 -0400

    add status for items in hisotry

--- routes/history.js
+++ routes/history.js
@@ -16,13 +16,14 @@ express().use(cookieSession({
 module.exports = (db) => {
 
   router.get("/", (req, res) => {
-    const query = `SELECT order_id, created_at::date AS date, menu_items.name AS item_name, price, quantity
+    const query = `SELECT order_id, created_at AS date, prep_time, menu_items.name AS item_name, price, quantity
       FROM order_items
       JOIN orders ON order_id = orders.id
       JOIN menu_items ON menu_item_id = menu_items.id
       WHERE customer_id = $1
-        AND created_at + prep_time < Now();`
-    const params = [req.session.userId] // replace with userID
+        AND created_at + prep_time < Now()
+      ORDER BY created_at DESC;`
+    const params = [req.session.userId] 
 
     db.query(query, params)
       .then(data => {
@@ -35,13 +36,14 @@ module.exports = (db) => {
             orders[orderId] = {
               orderId: order.order_id,
               date: order.date,
-              buyAgain: order.price * order.quantity
+              buyAgain: order.price * order.quantity,
+              status: order.date + order.prep_time < Date()? 'Completed' : 'In progress'
             };
           } else {
             orders[orderId].buyAgain += order.price * order.quantity;
           }
         }
-        const templateVars = { fullHistory, orders, userId: req.session.userId }; // will add userID later
+        const templateVars = { fullHistory, orders, userId: req.session.userId };
         console.log(templateVars)
         res.render("../views/history.ejs", templateVars)
       })
