aeb96530bac94d64a6ba80b2bda068afbd9470a0
Sean Jin <seanhoyeonjin@gmail.com>
2021-10-20 22:13:01 +0000

    revamp of the Twilio route files

--- routes/twilio.js
+++ routes/twilio.js
@@ -12,6 +12,7 @@ const client = require('twilio')(accountSid, authToken);
 
 module.exports = (db) => {
 
+  // POST Route for when the customer clicks on the "Order Now" button
   router.post("/", (req, res) => {
     const order = req.body.items;
     console.log(order);
@@ -31,30 +32,72 @@ module.exports = (db) => {
 
     });
 
-    router.post('/sms', (req, res) => {
-      const twiml = new MessagingResponse();
-      // access the message body and the number it was sent from
-      console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
-      const estimatedTimeMessage = req.body.Body;
+    // POST Route for when the restaurant owner specifies how long the order will take
+    router.post("/prep-time", (req, res) => {
+    // const order = req.body.items;
+    // console.log(order);
+    // let textMessage = "A customer has placed an order for ";
+    // for (const item of order) {
+    //   textMessage += `${item.quantity} ${item.name}! `
+    // }
+    // console.log(textMessage);
 
-      // this will send a message to the client once the owner responds with time
-      const clientMessage = `Your order will be ready in ${estimatedTimeMessage} minutes!`;
-      client.messages
-      .create({
-        body: clientMessage,
-        from: '+16137042914',
-        to: '+14372180544'
-       })
-       .then(message => console.log(message.sid));
+    // client.messages
+    // .create({
+    //    body: textMessage,
+    //    from: '+16137042914',
+    //    to: '+14372180544'
+    //  })
+    //  .then(message => console.log(message.sid));
 
-      // this will reply to the owner saying thank you!
-      const message = `
-      Thank you! We will let the customer know that it will take ${estimatedTimeMessage} minutes for the food to get ready.
-      `;
-      twiml.message(message);
-      res.writeHead(200, {'Content-Type': 'text/xml'});
-      res.end(twiml.toString());
     });
+
+    // POST Route for when the restaurant owner clicks on the Finished button
+    router.post("/finished", (req, res) => {
+    // const order = req.body.items;
+    // console.log(order);
+    // let textMessage = "A customer has placed an order for ";
+    // for (const item of order) {
+    //   textMessage += `${item.quantity} ${item.name}! `
+    // }
+    // console.log(textMessage);
+
+    // client.messages
+    // .create({
+    //    body: textMessage,
+    //    from: '+16137042914',
+    //    to: '+14372180544'
+    //  })
+    //  .then(message => console.log(message.sid));
+
+    });
+
+
+    // router.post('/sms', (req, res) => {
+    //   const twiml = new MessagingResponse();
+    //   // access the message body and the number it was sent from
+    //   console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
+    //   const estimatedTimeMessage = req.body.Body;
+
+    //   // this will send a message to the client once the owner responds with time
+    //   const clientMessage = `Your order will be ready in ${estimatedTimeMessage} minutes!`;
+    //   client.messages
+    //   .create({
+    //     body: clientMessage,
+    //     from: '+16137042914',
+    //     to: '+14372180544'
+    //    })
+    //    .then(message => console.log(message.sid));
+
+    //   // this will reply to the owner saying thank you!
+    //   const message = `
+    //   Thank you! We will let the customer know that it will take ${estimatedTimeMessage} minutes for the food to get ready.
+    //   `;
+
+    //   twiml.message(message);
+    //   res.writeHead(200, {'Content-Type': 'text/xml'});
+    //   res.end(twiml.toString());
+    // });
 
   return router;
 }
