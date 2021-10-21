4311a2170c31c0280fbb9d9f623266126ceba896
Sean Jin <seanhoyeonjin@gmail.com>
2021-10-21 00:42:54 +0000

    changes

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 INSERT INTO customers (name, email, phone_number, password, address, city, province, postal_code, member_since, is_active)
-VALUES 
-('Alejandro Osborne',  'ariaatkinson@outlook.com', '+1 888 923-6532', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '1246 Kisom Pike', 'Brandon', 'Manitoba', '45193', '2021-02-03 10:33:21.00', TRUE),
+VALUES
+('Alejandro Osborne',  'ariaatkinson@outlook.com', '+14372180544', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '1246 Kisom Pike', 'Brandon', 'Manitoba', '45193', '2021-02-03 10:33:21.00', TRUE),
 ('Estelle Walsh', 'elistanton@yahoo.com', '+1 862 578-8623', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '672 Nicdum Mill', 'St. Albert', 'Alberta', '52505', '2021-03-23 08:32:01.00', TRUE),
 ('Herbert Graves', 'emilyowen@live.com', '+1 358 872-0963', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '847 Hepiw Mill', 'Carcross', 'Yukon', '98992', '2021-01-18 11:00:17.00', TRUE),
 ('Isabelle Robbins', 'miasutton@aol.com', '+1 892 734-9783', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '1823 Gafit Parkway', 'Coaticook', 'Quebec', '14821', '2021-01-01 11:22:33.00', TRUE);
